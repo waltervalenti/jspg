01985ec587ca59771a5a00b30678e7671db5c844
walter <walter@valx>
2022-11-24 15:05:00 +0100

    first stable release

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # jspg
 
 jspg in a simple wrapper of gpg in JSON format.
+
+Why use jspg ? .. Why not ? !!!
+
+Usage:
+comandi: sign|enc|sign-enc|verify|dec|help
+default: file jspg
+
+sign: firma un file. crea file.jspg
+enc:  cifra un file. crea file.jspg
+sign-enc: firma e cifra un file. crea crea file.jspg
+verify: verifica le firme in file.jspg
+dec: decifra file.jspg e crea file
+file.jspg: azione di default. prova decifrare e vefificare le firme se presenti
+in tutti i casi, se un file esiste già, viene creato un nuovo file con .NUM nel nome
+
+
--- jspg.py
+++ jspg.py
@@ -6,7 +6,8 @@ from dataclasses_json import dataclass_json
 import gpg, sys
 import base64
 from gpg.constants.sig import mode
-
+from os.path import exists
+import re
 
 @dataclass_json
 @dataclass
@@ -28,7 +29,8 @@ class SignEnc:
 class Jspg:
     def __init__(self):
         if len(sys.argv) == 1:
-            print("comandi: sign|enc|sign-enc|verify|dec")
+            print("comandi: sign|enc|sign-enc|verify|dec|help")
+            print("default: file jspg")
             sys.exit(-1)
         cmd = sys.argv[1]
         state =False
@@ -59,48 +61,70 @@ class Jspg:
             self.sign_enc(id_keys_sign, id_keys_enc, input_file)
         if cmd == "verify":
             state = True
-            if len(sys.argv) != 3:
-                print("Uso: verify [file]")
+            if len(sys.argv) != 3 or not str.endswith(sys.argv[2], ".jspg"):
+                print("Uso: verify [file.jspg]")
                 sys.exit(-1)
             input_file = sys.argv[2]
             self.verify(input_file)
         if cmd == "dec":
             state = True
-            if len(sys.argv) != 3:
-                print("Uso: dec [file]")
+            if len(sys.argv) != 3 or not str.endswith(sys.argv[2], ".jspg"):
+                print("Uso: dec [file.jspg]")
                 sys.exit(-1)
             input_file = sys.argv[2]
             self.decrypt(input_file)
+        if len(sys.argv) == 2 and cmd.endswith(".jspg"):
+            state = True
+            input_file = cmd
+            self.default(input_file)
+        if cmd == "help" and len(sys.argv) == 2:
+            state = True
+            self.help()
         if state == False:
-            print("comando %s sconosciuto", cmd)
+            print(f"comando \"{cmd}\" sconosciuto")
 
     def sign(self, id_keys, input_file):
         with open(input_file, "rb") as f:
             signature = self.__impl_sign(id_keys, input_file)
             data = Signatures(base64.b64encode(f.read()).decode("utf-8"), base64.b64encode(signature).decode("utf-8"))
-            print(data.to_json())
+            #print(data.to_json())
+            self.__write_jspg(input_file, data.to_json())
             
     def encrypt(self, id_keys, input_file):
         cypher = self.__impl_enc(id_keys, input_file)
         data = Encrypt(base64.b64encode(cypher).decode("utf-8"))
-        print(data.to_json())
+        #print(data.to_json())
+        self.__write_jspg(input_file, data.to_json())
                 
     def sign_enc(self, id_keys_sign, id_keys_enc, input_file):
         signature  = self.__impl_sign(id_keys_sign, input_file)
         cypher = self.__impl_enc(id_keys_enc, input_file)
         data = SignEnc(base64.b64encode(cypher).decode("utf-8"), base64.b64encode(signature).decode("utf-8"))
-        print(data.to_json())
+        #print(data.to_json())
+        self.__write_jspg(input_file, data.to_json())
         
     def verify(self, input_file):
         c = gpg.Context()
-        f = json.load(open(input_file))
+        try:
+            f = json.load(open(input_file))
+        except:
+            print(f"file \"{input_file}\" non valido")
+            sys.exit(-1)
         enc = False
         try:
-            plain = f['plain']
+            plain = base64.b64decode(f['plain'])
         except:
-            encrypted = f['encrypted']
-            enc = True
-        signatures = f['signatures']
+            try:
+                encrypted = f['encrypted']
+                enc = True
+            except:
+                print('"plain" o "encrypted" non presenti')
+                sys.exit(-1)
+        try:
+            signatures = f['signatures']
+        except:
+            print('"signatures" non presente')
+            sys.exit(-1)
         if enc == True:
             plain, _, _ = c.decrypt(base64.b64decode(encrypted))
         data, result = c.verify(plain, base64.b64decode(signatures))
@@ -109,11 +133,62 @@ class Jspg:
             
     def decrypt(self, input_file):
         c = gpg.Context()
-        f = json.load(open(input_file))
-        encrypted = f['encrypted']
-        plainfile, _, _ = c.decrypt(base64.b64decode(encrypted))
-        sys.stdout.buffer.write(plainfile)
+        try:
+            f = json.load(open(input_file))
+        except:
+            print(f"file \"{input_file}\" non valido")
+            sys.exit(-1)
+        try:
+            encrypted = f['encrypted']
+        except:
+            print('"encrypted" non presente')
+            sys.exit(-1)
+        plain, _, _ = c.decrypt(base64.b64decode(encrypted))
+        #sys.stdout.buffer.write(plain)
+        self.__write_plain(input_file, plain)
+
+    def default(self, input_file):
+        c = gpg.Context()
+        try:
+            f = json.load(open(input_file))
+        except:
+            print(f"file \"{input_file}\" non valido")
+            sys.exit(-1)
+        enc = True
+        signed = False
+        try:
+            encrypted = f['encrypted']
+        except:
+            try:
+                plain = base64.b64decode(f['plain'])
+            except:
+                print('"plain" o "encrypted" non presenti')
+                sys.exit(-1)
+            enc = False
+        if enc == True:
+            plain, _, _ = c.decrypt(base64.b64decode(encrypted))
+            #sys.stdout.buffer.write(plain)
+            self.__write_plain(input_file, plain)
+        try:
+            signatures = f['signatures']
+            signed = True
+        except:
+            pass
+        if signed == True:
+            data, result = c.verify(plain, base64.b64decode(signatures))
+            for sign in result.signatures:
+                print(sign.fpr, c.get_key(sign.fpr).uids[0].uid)
                 
+    def help(self):
+        print("sign: firma un file. crea file.jspg")
+        print("enc:  cifra un file. crea file.jspg")
+        print("sign-enc: firma e cifra un file. crea crea file.jspg")
+        print("verify: verifica le firme in file.jspg")
+        print("dec: decifra file.jspg e crea file")
+        print("file.jspg: azione di default. prova decifrare e vefificare le firme se presenti")
+        print("in tutti i casi, se un file esiste già, viene creato un nuovo file con .NUM nel nome")
+        
+        
     def __impl_sign(self, id_keys, input_file):
         keys = []
         for k in id_keys.split(","):
@@ -133,6 +208,41 @@ class Jspg:
             with open(input_file, "rb") as f:
                 cypher, _, _ = c.encrypt(f.read(), recipients, sign=False)
                 return cypher
+        
+    def __write_jspg(self, input_file, content, Num = 0):
+        if Num == 0:
+            rip = ""
+        else:
+            rip = "." + str(Num)
+        output_file = input_file + rip + ".jspg"
+        if exists(output_file):
+            self.__write_jspg(input_file, content, Num = Num + 1)
+            return 0
+        f = open(output_file, "w")
+        f.write(content)
+        f.close()
+        
+    def __write_plain(self, input_file, content, Num = 0):
+        if Num == 0:
+            rip = []
+        else:
+            rip = [str(Num)]
+        input_file_splitted = str.split(input_file, ".")
+        l = len(input_file_splitted)
+        if l > 2:
+            head = input_file_splitted[0: l - 2]
+            tail = [input_file_splitted[l - 2]]
+        else:
+            head = [input_file_splitted[0]]
+            tail = []
+        output_file = str.join("." , head + rip +  tail)
+        if exists(output_file):
+            self.__write_plain(input_file, content, Num = Num + 1)
+            return 0
+        f = open(output_file, "wb")
+        f.write(content)
+        f.close()
+ 
         
 ######################################################################################################################
 
